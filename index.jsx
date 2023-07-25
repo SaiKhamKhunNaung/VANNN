f9f6266852db2f9dda708739b5a3bfd0eb61d4ae
SaiSaiKhamKhunNaung <saikhamkhunnaung616@gmail.com>
2023-07-26 01:46:02 +0630

    d

--- index.jsx
+++ index.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 
 function App() {
-
   return (
     <BrowserRouter>
       <header>
@@ -20,4 +21,6 @@ function App() {
   )
 }
 
-export default App
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
